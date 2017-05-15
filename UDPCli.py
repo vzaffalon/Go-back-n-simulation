0a90270c451587c1d9bcc6046ab484317bc33854
vzaffalon <zaffalonvictor@gmail.com>
2017-05-15 17:56:19 -0300

    go-back-n simulation implemented and gitgnore file

--- UDPCli.py
+++ UDPCli.py
@@ -4,14 +4,61 @@ from Message import *
 import time
 import json
 
-def receiveAck():
-        try:
-            ackSerialized, serverAddress = clientSocket.recvfrom(2048) #buffer size 2048
-            ack = json.loads(ackSerialized)
-            print "Ack recebido: " + str(ack)
-        except socket.timeout:
-            print 'ack reception timed out'
+#socket config variables
+serverIp = ''
+serverPort = 12000
+timeOut = 5     #em segundos
 
+#client socket configuration
+clientSocket = socket(AF_INET, SOCK_DGRAM)          #conexao socket tipo internet e udp
+clientSocket.settimeout(timeOut)
+
+#go-back-n variables
+#parametros modificaveis
+windowSize = 5   #janela do cliente
+timeBetweenPackets = 3 #tempo entre envio de pacotes em segundos
+packetsEmitedPerMinute = 60   #quantidade de pacotes emitidos por minuto
+maximumPacketFlow = 70    #quantidade maxima de pacotes que a rede suporta ate ser considerada congestionada
+numbersOfPacketsToBeTransmited = 47    #numero de mensagens que serao transmitidos pelo programa
+messageString = "hello server "   #mensagem padrao que sera enviada
+
+#parametros nao modificaveis
+sequenceNumber = 1       #numero de sequencia da mensagem
+expectedSequenceNumber = 1 #numero do sequence number que o cliente espera receber do ack
+window = []      #lista contendo elementos atualmente na janela
+errorAlreadyOcurred = False  #evita que mensagems de error seja printada denovo pelos proximos acks da janela enviada
+
+def receiveAck():
+        ackSerialized, serverAddress = clientSocket.recvfrom(2048) #buffer size 2048
+        ack = json.loads(ackSerialized)
+        verifySequenceNumber(ack['sequenceNumber'],ack)
+
+def verifySequenceNumber(seqNumber,ack):
+    global expectedSequenceNumber,errorAlreadyOcurred
+    if expectedSequenceNumber == seqNumber:
+        if errorAlreadyOcurred == False: #verifica se os proximos acks devem ser ignorados ou nao
+            print "Ack recebido: " + str(ack) + '\n'
+            moveWindow()
+        else:
+            print "Ack " + str(ack) +' recebido e ignorado' +'\n'
+    else:
+        if errorAlreadyOcurred == False: #evita que mensagems de error seja printada denovo pelos proximos acks da janela enviada
+            if seqNumber == -1:
+                time.sleep(timeOut) #simula timeout
+                print 'timeout - ack do sequenceNumber ' + str(expectedSequenceNumber) + ' nao foi recebido - reenviando janela' + '\n'
+                errorAlreadyOcurred = True
+            else:
+                print 'numero de sequencia errado - reenviando janela' + '\n'
+                errorAlreadyOcurred = True
+
+def moveWindow():
+    global expectedSequenceNumber,numbersOfPacketsToBeTransmited
+    removeMessageFromWindow()           #mensagem enviada e com ack recebido retira da janela
+    expectedSequenceNumber = expectedSequenceNumber + 1     #numero usado para verificacao de ordem
+
+    if numbersOfPacketsToBeTransmited > 0:               #verifica se ainda existem pacotes a serem transmitidos
+        addNewMessageToWindow()
+        numbersOfPacketsToBeTransmited = numbersOfPacketsToBeTransmited - 1
 
 
 def removeMessageFromWindow():
@@ -20,34 +67,13 @@ def removeMessageFromWindow():
 def sendMessage(clientMessage):
     messageSerialization = json.dumps(clientMessage.__dict__)       #tranforma classe pra dicionario e manda em formato json
     clientSocket.sendto(messageSerialization,(serverIp, serverPort))
-    print "Mensagem enviada: " + str(clientMessage.__dict__)
+    print "Mensagem enviada: " + str(clientMessage.__dict__) + '\n'
 
 def addNewMessageToWindow():
     global sequenceNumber
     clientMessage = Message(sequenceNumber,messageString + str(sequenceNumber))
     window.append(clientMessage)
     sequenceNumber = sequenceNumber + 1
-
-
-#socket config variables
-serverIp = ''
-serverPort = 12000
-timeOut = 11     #em segundos
-
-#client socket configuration
-clientSocket = socket(AF_INET, SOCK_DGRAM)          #conexao socket tipo internet e udp
-clientSocket.settimeout(timeOut)
-
-#go-back-n variables
-windowSize = 5   #janela do cliente
-timeBetweenPackets = 1 #tempo entre envio de pacotes em segundos
-packetsEmitedPerMinute = 60   #quantidade de pacotes emitidos por minuto
-maximumPacketFlow = 70    #quantidade maxima de pacotes que a rede suporta ate ser considerada congestionada
-expectedSequenceNumber = 0 #numero do sequency number que o cliente espera receber do ack
-numbersOfPacketsToBeTransmited = 47    #numero de mensagens que serao transmitidos pelo programa
-messageString = "hello server "   #mensagem padrao que sera enviada
-sequenceNumber = 1       #numero de sequencia da mensagem
-window = []      #lista contendo elementos atualmente na janela
 
 #inicia a janela com as primeiras mensagens a serem enviadas
 for x in range (0,windowSize):
@@ -57,20 +83,13 @@ for x in range (0,windowSize):
 #envia mensagens,recebe o ack,retira a primeira mensagem e adiciona a proxima na janela
 while len(window) > 0:
         windowLen = len(window)
+        errorAlreadyOcurred = False
         for y in range(0,windowLen):
-            sendMessage(window[y])
+            sendMessage(window[y])                  #envia as mensagens da janela
             time.sleep(timeBetweenPackets)          #gera um tempo entre envio de pacotes
 
         for z in range(0,windowLen):
-            receiveAck()
-            removeMessageFromWindow()           #mensagem enviada e com ack recebido retira da janela
-            expectedSequenceNumber = expectedSequenceNumber + 1     #numero usado para verificacao de ordem
-
-            if numbersOfPacketsToBeTransmited > 0:               #verifica se ainda existem pacotes a serem transmitidos
-                addNewMessageToWindow()
-                numbersOfPacketsToBeTransmited = numbersOfPacketsToBeTransmited - 1
+            receiveAck()                            #recebe os ack da janela enviada
             time.sleep(timeBetweenPackets)          #gera um tempo entre envio de pacotes
-
-
 
 clientSocket.close()
--- UDPSrv.py
+++ UDPSrv.py
@@ -3,13 +3,21 @@ from Ack import *
 from Message import *
 import time
 import json
-clientAddress = 0
+import random
+
+#global variables
+clientAddress = 0    #variavel que recebera endereco do cliente
+randomChance = 10    #valor em porcentagem da chance de erros ocorrerem
+timeBetweenAcksSend = 3   #tempo em segundos entre envio de acks
 
 def sendAck(sequenceNumber,data):
     ack = Ack(sequenceNumber,data + " ack")
     ackSerialization = json.dumps(ack.__dict__)  #envia o ack em formato json
     serverSocket.sendto(ackSerialization, clientAddress)
-    print "Ack Enviado: " + str(ack.sequenceNumber)
+    if sequenceNumber == -1:
+        print "Ack com falha enviado: " + str(ack.__dict__) + '\n'
+    else:
+        print "Ack enviado: " + str(ack.__dict__) + '\n'
 
 
 def receiveMessage():
@@ -17,9 +25,27 @@ def receiveMessage():
     messageSerialized, address = serverSocket.recvfrom(2048)
     clientAddress = address
     message = json.loads(messageSerialized)  #recebe a mensagem em formato json
-    print "Mensagem recebida " + str(message)
+    print "Mensagem recebida " + str(message) + '\n'
     return message
 
+def generateRandomChanceOfFail():
+    global randomChance
+    if random.randrange(0,100) < randomChance:
+        return True
+    else:
+        return False
+
+def verifyAckFailures():
+    if generateRandomChanceOfFail() == True:
+        #envia mensagem com falha de ack ou seja sequenceNumber -1 gerando timeout
+        sendAck(-1,message['data'])
+    else:
+        if generateRandomChanceOfFail() == True:
+            #envia mensagem na ordem incorreta ou seja envia sequenceNumber errado
+            sendAck(message['sequenceNumber'] + 1,message['data'])
+        else:
+            #nenhum erro envia mensagem normalmente com sequenceNumber correto
+            sendAck(message['sequenceNumber'],message['data'])
 
 #socket config variables
 serverIp = ''
@@ -28,11 +54,9 @@ serverPort = 12000
 #server socket configuration
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind((serverIp, serverPort))
-print 'The server is ready to receive'
-
-timeBetweenAcksSend = 1
+print 'The server is ready to receive' + '\n'
 
 while 1:
     time.sleep(timeBetweenAcksSend)          #gera um tempo entre envio de pacotes
     message = receiveMessage()
-    sendAck(message['sequenceNumber'],message['data'])
+    verifyAckFailures()
